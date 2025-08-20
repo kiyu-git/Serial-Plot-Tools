ecc74da08e67a817af68048b6c11b9d572bfb664
kiyu-git <47422498+kiyu-git@users.noreply.github.com>
2025-08-20 23:33:21 +0900

    fix: ESLint errors in renderer files

--- src/renderer/pages/DataViewer/index.tsx
+++ src/renderer/pages/DataViewer/index.tsx
@@ -1,6 +1,6 @@
 import { DownloadIcon } from '@chakra-ui/icons';
 import { Box, Center, Grid, GridItem, Heading, Text } from '@chakra-ui/react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Plot from 'react-plotly.js';
 import styles from './index.module.scss';
 
@@ -10,9 +10,23 @@ type plotData = {
   title: string;
 };
 
-export function DataViewer() {
+export default function DataViewer() {
   const [plotData, setPlotData] = useState<plotData[]>([]);
   const [fileName, setFileName] = useState<string>('');
+
+  const showPlot = async (path: string) => {
+    setFileName(path);
+    const [header, data] = await window.api.loadData(path);
+    const formatData: plotData[] = [];
+    const datetimeData = data[0].map((strDatetime: string) => {
+      return new Date(strDatetime);
+    });
+    for (let i = 1; i < header.length; i++) {
+      formatData.push({ title: header[i], x: datetimeData, y: data[i] });
+    }
+
+    setPlotData(formatData);
+  };
 
   const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.stopPropagation();
@@ -32,48 +46,34 @@ export function DataViewer() {
     showPlot(filepath);
   };
 
-  const showPlot = async (path: string) => {
-    setFileName(path);
-    const [header, data] = await window.api.loadData(path);
-    const formatData: plotData[] = [];
-    const datetime_data = data[0].map((str_datetime: string) => {
-      return new Date(str_datetime);
-    });
-    for (let i = 1; i < header.length; i++) {
-      formatData.push({ title: header[i], x: datetime_data, y: data[i] });
-    }
-
-    setPlotData(formatData);
-  };
-
   return (
     <Grid
       mx={3}
       templateAreas={`"header header" "nav main"`}
-      gridTemplateRows={'auto 1fr'}
-      gridTemplateColumns={'20% 1fr'}
-      h={'100vh'}
+      gridTemplateRows="auto 1fr"
+      gridTemplateColumns="20% 1fr"
+      h="100vh"
       margin={0}
       padding={2}
     >
-      <GridItem area={'header'} margin={'0 0 1em 0'}>
+      <GridItem area="header" margin="0 0 1em 0">
         <Heading>Data Viewer</Heading>
         <Text>Realtime Data Loggerで保存したデータをグラフにします。</Text>
       </GridItem>
-      <GridItem area={'nav'}>
-        <Box bgColor={'white'}>
-          <Heading fontSize={'xl'} mb={1}>
+      <GridItem area="nav">
+        <Box bgColor="white">
+          <Heading fontSize="xl" mb={1}>
             Select Data File
           </Heading>
           <Box
-            w={'100%'}
+            w="100%"
             cursor="pointer"
             padding={1}
             borderWidth="1px"
             borderRadius="md"
-            color={'teal.600'}
-            borderColor={'teal.600'}
-            minH={'150px'}
+            color="teal.600"
+            borderColor="teal.600"
+            minH="150px"
             _hover={{ background: 'teal.50' }}
             onDragOver={(e) => onDragOver(e)}
             onDrop={(e) => onDrop(e)}
@@ -84,12 +84,12 @@ export function DataViewer() {
               <Center>
                 <DownloadIcon boxSize={5} m={2} />
               </Center>
-              <Text fontSize={'sm'}>{fileName}</Text>
+              <Text fontSize="sm">{fileName}</Text>
             </Box>
           </Box>
         </Box>
       </GridItem>
-      <GridItem bg="" overflowX={'auto'} overflowY={'scroll'} area={'main'}>
+      <GridItem bg="" overflowX="auto" overflowY="scroll" area="main">
         {plotData?.map((data) => {
           return (
             <Plot
